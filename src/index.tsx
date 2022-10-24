2832815182797b3adf25c70a8ce620f777103d75
Alucard <minxter.yt@gmail.com>
2022-10-24 17:57:39 +0100

    Quick Fixes
    ID on icon head element like in unmerged pr
    Fix of default Icon
    Removal of uneeded icon and manifest keys
    Cleanup of onBlur()

--- src/index.tsx
+++ src/index.tsx
@@ -5,23 +5,20 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 const blurTimer = setTimeout(() => {})
+const favicon = document.getElementById('favicon')
 
 const onBlur = () => {
   clearTimeout(blurTimer)
   setTimeout(() => {
     document.title = "Asleep - cstanze"
-    let favicon = document.getElementById('favicon')
-    if(!favicon) return
-    favicon.href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💤</text></svg>" 
-    }, 5000)
+    favicon?.setAttribute("href", "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💤</text></svg>")
+  }, 5000)
 }
 
 const onFocus = () => {
   clearTimeout(blurTimer)
   document.title = "Awake - cstanze"
-  let favicon = document.getElementById('favicon')
-  if(!favicon) return
-  favicon.href=href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌱</text></svg>"
+  favicon?.setAttribute("href", "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌱</text></svg>" )
 }
 
 window.addEventListener('blur', onBlur)
